453adc0be60fc399cd94595d9640cb3f73c3c56f
vahid farjood <vahid.farjood@gmail.com>
2022-07-12 21:43:39 +0200

    add result.txt to the program

---
+++ build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/dev/code/yolov5-arm/src/main.cpp" "/home/pi/dev/code/yolov5-arm/build/CMakeFiles/app.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../lib/opencv-4/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
